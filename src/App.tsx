9607f0c1d52be26cbd6ecf83e5fe7a403b4f5670
syamozipc <syamozipc@gmail.com>
2023-04-20 09:33:22 +0900

    totorial 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,22 @@ export default function App() {
   const {
     register,
     handleSubmit,
+    watch,
     // formの状態を確認可能
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    // 初期表示
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+    },
+  });
   renderCount++;
-  // バリデーションエラーが格納されている
+  // バリデーションエラーが格納されている（バリデーションの種類, 設定したエラーメッセージ, DOM要素）
   console.log(errors);
+
+  // firstName入力の都度、値を取得している（引数を省略するとform全体を監視）
+  const firstName = watch("firstName");
 
   return (
     <div>
@@ -30,13 +40,26 @@ export default function App() {
       >
         {/* バリデーション通らないとsubmitされない */}
         <input
-          {...register("firstName", { required: true })}
+          // required: trueだけでもOK（エラーメッセージは空文字になる）
+          // {...register("firstName", { required: true })}
+          {...register("firstName", { required: "This is required" })}
           placeholder="First Name"
         />
+        {/* 1度submitした後は、入力の度に検証されるので、 リアルタイムで値の妥当性が分かる*/}
+        <p>{firstName}</p>
+        <p>{errors.firstName?.message}</p>
         <input
-          {...register("lastName", { required: true, minLength: 4 })}
+          {...register("lastName", {
+            required: "This is required",
+            // minLength: 4 でもOK
+            minLength: {
+              value: 4,
+              message: "Min length is 4",
+            },
+          })}
           placeholder="Last Name"
         />
+        <p>{errors.lastName?.message}</p>
         <input type="submit" />
       </form>
     </div>
